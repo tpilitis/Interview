f694194f7878dae1a619f5cefadf21e503067b3d
Teodora Pilitis <teodora.pilitis@gmail.com>
2024-05-16 12:54:46 +0300

    Sample of Dijkstra Algorithm

---
+++ BFS/DijkstraAlgorithmService.cs
@@ -0,0 +1,65 @@
+﻿namespace Interview.Samples.Application
+{
+    public class DijkstraAlgorithmService
+    {
+        private const int MaxIndex = 99999999;
+
+        /// <summary>
+        /// https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/?ref=lbp
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <param name="source"></param>
+        /// <param name="totalNodes"></param>
+        public int[] CalculatesShortesPath(int[,] graph, int source, int totalNodes)
+        {
+            int[] nodeDistances = new int[totalNodes];
+
+            bool[] visitedNodes = new bool[totalNodes];
+
+            for (int i = 0; i < totalNodes; i++)
+            {
+                nodeDistances[i] = MaxIndex;
+            }
+
+            nodeDistances[source] = 0;
+
+            for (int count = 0; count < totalNodes; count++)
+            {
+                int closestNodeIndex = GetClosestUnvisitedNodeIndex(nodeDistances, visitedNodes, totalNodes);
+
+                visitedNodes[closestNodeIndex] = true;
+
+                for (int index = 0; index < totalNodes; index++)
+                {
+                    if (!visitedNodes[index]
+                        // has link
+                        && graph[closestNodeIndex, index] != 0
+                        // the min distance is shorter than the distance of the current node
+                        && nodeDistances[closestNodeIndex] + graph[closestNodeIndex, index] < nodeDistances[index])
+                    {
+                        nodeDistances[index] = nodeDistances[closestNodeIndex] + graph[closestNodeIndex, index];
+                    }
+                }
+            }
+
+            return nodeDistances;
+        }
+
+        private int GetClosestUnvisitedNodeIndex(int[] nodeDistances, bool[] visitedNodes, int totalNodes)
+        {
+            int minNodeIndex = -1;
+            int nodeDistance = MaxIndex;
+
+            for (int i = 0; i < totalNodes; i++)
+            {
+                if (!visitedNodes[i] && nodeDistances[i] <= nodeDistance)
+                {
+                    nodeDistance = nodeDistances[i];
+                    minNodeIndex = i;
+                }
+            }
+
+            return minNodeIndex;
+        }
+    }
+}
---
+++ Interview.Samples.Tests/DijkstraAlgorithmTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Interview.Samples.Application;
+
+namespace Interview.Samples.Tests
+{
+    [TestClass]
+    public class DijkstraAlgorithmTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            int[,] graph
+            = new int[,] {
+                { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
+                { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
+                { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
+                { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
+                { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+                { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
+                { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
+                { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
+                { 0, 0, 2, 0, 0, 0, 6, 7, 0 }
+            };
+
+            var serviceUnderTest = new DijkstraAlgorithmService();
+            var distances = serviceUnderTest.CalculatesShortesPath(graph, 0, 9);
+
+            distances.Should().BeEquivalentTo(new[] { 0, 4, 12, 19, 21, 11, 9, 8, 14 });
+        }
+    }
+}
