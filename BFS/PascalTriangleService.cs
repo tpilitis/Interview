cb4576d5cc37f8aba0ed3b8d24be463e138caf1f
Teodora Pilitis <teodora.pilitis@gmail.com>
2024-05-16 10:31:48 +0300

    Add pascal triangle example & tests

---
+++ BFS/PascalTriangleService.cs
@@ -0,0 +1,43 @@
+﻿namespace Interview.Samples.Application
+{
+    /// <summary>
+    /// https://leetcode.com/problems/pascals-triangle/description/
+    /// </summary>
+    public static class PascalTriangleService
+    {
+        public static int[][] GenerateTriangle(int rows)
+        {
+            if (rows < 1 || rows >= 30)
+            {
+                return Array.Empty<int[]>();
+            }
+
+            var pascalTriangle = new int[rows][];
+
+            // 1st element is always 1            
+            pascalTriangle[0] = [1];
+
+            for (int i = 1; i < rows; i++)
+            {
+                pascalTriangle[i] = new int[i + 1];
+
+                // 1st element always is 1
+                pascalTriangle[i][0] = 1;
+                for (int j = 1; j < i+1; j++)
+                {
+                    // last element
+                    if (j == i)
+                    {
+                        pascalTriangle[i][j] = 1;
+                    }
+                    else
+                    {
+                        pascalTriangle[i][j] = pascalTriangle[i - 1][j - 1] + pascalTriangle[i - 1][j];
+                    }
+                }
+            }
+
+            return pascalTriangle;
+        }
+    }
+}
---
+++ Interview.Samples.Tests/PascalTriangleTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Interview.Samples.Application;
+
+namespace Interview.Samples.Tests
+{
+    [TestClass]
+    public class PascalTriangleTests
+    {
+        [TestMethod]
+        public void AssertPascalTriangle()
+        {
+            const int rows = 5;
+            var triangleResult = PascalTriangleService.GenerateTriangle(rows);
+
+            triangleResult.GetLength(0).Should().Be(rows);
+
+            triangleResult[0].Should().BeEquivalentTo([1]);
+            triangleResult[1].Should().BeEquivalentTo([1,1]);
+            triangleResult[2].Should().BeEquivalentTo([1,2,1]);
+            triangleResult[3].Should().BeEquivalentTo([1,3,3,1]);
+            triangleResult[4].Should().BeEquivalentTo([1,4,6,4,1]);
+        }
+
+        [DataTestMethod]
+        [DataRow(0)]
+        [DataRow(31)]
+        public void AssertConstrainRowCannotBeLessThanOne(int rows)
+        {
+            var triangleResult = PascalTriangleService.GenerateTriangle(rows);
+
+            triangleResult.Should().BeEmpty();
+        }
+    }
+}
