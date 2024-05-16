c0ff6686d90d93b4eac37e08dea186963ee23a66
Teodora Pilitis <teodora.pilitis@gmail.com>
2024-05-16 16:33:33 +0300

    Num Array sample & test

---
+++ BFS/Arrays/NumArray.cs
@@ -0,0 +1,28 @@
+﻿namespace Interview.Samples.Application.Arrays
+{
+    /// <summary>
+    /// https://leetcode.com/problems/range-sum-query-immutable
+    /// </summary>
+    public class NumArray
+    {
+        private readonly int[] prefixSum;
+
+        public NumArray(int[] array)
+        {
+            prefixSum = new int[array.Length + 1];
+
+            prefixSum[0] = array[1] + array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                prefixSum[i + 1] = prefixSum[i] + array[i];
+            }
+        }
+
+        public int SumRange(int left, int rigth)
+        {
+            var sum = prefixSum[rigth +1] - prefixSum[left];
+            
+            return sum;
+        }
+    }
+}
---
+++ Interview.Samples.Tests/ArrayTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Interview.Samples.Application.Arrays;
+
+namespace Interview.Samples.Tests
+{
+    [TestClass]
+    public class ArrayTests
+    {
+        [DataTestMethod]
+        [DataRow(0, 2, 1)]
+        [DataRow(2, 5, -1)]
+        [DataRow(0, 5, -3)]
+        public void Test_RangeSum_Immutable(int left, int rigth, int expectedSum)
+        {
+            var numArray = new NumArray([-2, 0, 3, - 5, 2, -1]);
+            
+            var result = numArray.SumRange(left, rigth);
+            result.Should().Be(expectedSum);
+        }
+    }
+}
