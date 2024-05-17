9ddbecb2a3e479bd5a909939777c5059325a9896
Teodora Pilitis <teodora.pilitis@gmail.com>
2024-05-17 17:10:34 +0300

    wordie add some validation

--- Wordle/Program.cs
+++ Wordle/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Wordle
 {
@@ -6,8 +7,12 @@ namespace Wordle
     {
         static void Main(string[] args)
         {
-            var maxAttempts = 3;
-            var secretWord = "crown";
+            var maxAttempts = 6;
+            var secretWords = new[] { "apple", "crown", "dress", "sport" };
+
+            var random = new Random();
+            var wordIndex = random.Next(secretWords.Length - 1);
+            var secretWord = secretWords[wordIndex];
 
             Console.WriteLine("Guess the 5 length word?");
             Console.WriteLine($"You have {maxAttempts} attempts.");
@@ -15,15 +20,31 @@ namespace Wordle
             var wordie = new Word();
 
             var attempts = 0;
+
             while (attempts < maxAttempts)
             {
                 attempts++;
 
                 var guessWord = Console.ReadLine();
+                if (guessWord.Length != secretWord.Length)
+                {
+                    Console.WriteLine($"Please try with valid word. Max length: {secretWord.Length}. Press: 'Y' to continue.");
+                    var answer = Console.ReadLine();
+                    if (answer == "Y")
+                    {
+                        attempts--;
+                        Console.WriteLine("Give me a word:");
+                        guessWord = Console.ReadLine();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
                 var resultWord = wordie.Match(guessWord, secretWord);
 
                 Console.WriteLine(resultWord);
-              
+
                 if (wordie.IsFullMatch)
                 {
                     Console.WriteLine($"You found the world from {attempts} attempt.");
