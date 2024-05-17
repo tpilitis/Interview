dfd712ed819321de186033a68842b11d251cb745
Teodora Pilitis <teodora.pilitis@gmail.com>
2024-05-17 16:46:28 +0300

    create wordie game

---
+++ Wordle/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Wordle
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var maxAttempts = 3;
+            var secretWord = "crown";
+
+            Console.WriteLine("Guess the 5 length word?");
+            Console.WriteLine($"You have {maxAttempts} attempts.");
+
+            var wordie = new Word();
+
+            var attempts = 0;
+            while (attempts < maxAttempts)
+            {
+                attempts++;
+
+                var guessWord = Console.ReadLine();
+                var resultWord = wordie.Match(guessWord, secretWord);
+
+                Console.WriteLine(resultWord);
+              
+                if (wordie.IsFullMatch)
+                {
+                    Console.WriteLine($"You found the world from {attempts} attempt.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Please try again. {maxAttempts - attempts} attempts to go :)");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Wordle/Word.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace Wordle
+{
+    public class Word
+    {
+        public bool IsFullMatch { get; private set; }
+
+        public string Match(string inputWord, string secretWord)
+        {
+            if (inputWord.Equals(secretWord))
+            {
+                IsFullMatch = true;
+                return string.Join("|", secretWord.ToCharArray());
+            }
+
+            var resultWord = new StringBuilder();
+            var secretWorld = secretWord.ToArray();
+            for (int i = 0; i < inputWord.Length; i++)
+            {
+                if (inputWord[i] == secretWorld[i])
+                {
+                    // matching position
+                    resultWord.Append($"|{inputWord[i]}|");
+                }
+                else
+                {
+                    if (secretWord.Contains(inputWord[i]))
+                    {
+                        resultWord.Append($"<{inputWord[i]}>");
+                    }
+                    else
+                    {
+                        resultWord.Append(inputWord[i]);
+                    }
+                }
+            }
+
+            resultWord.ToString();
+
+            return resultWord.ToString();
+        }
+    }
+}
